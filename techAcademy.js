8ef4e8306b14a03747b10cddf5648c3cdeb1d38e
hokkeee <100758032+hokkeee@users.noreply.github.com>
2022-03-14 15:12:20 +0900

    Delete techAcademy.js

--- techAcademy.js
+++
@@ -1,9 +0,0 @@
-$("#btn1").on("click", function()  {
-    let date = new Date();
-    let year = date.getFullYear();
-    let month = date.getMonth() + 1;
-    let day = date.getDate();
-    let message = `今日は${year}年${month}月${day}日です`;
-    $("#tBox").val(message);
-  });
-  
